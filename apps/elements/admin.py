08e22103e85396500345405e32d117b7a26b25c8
masozee <nurojilukmansyah@gmail.com>
2023-08-22 13:41:13 +0700

    add models

--- apps/elements/admin.py
+++ apps/elements/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
+from .models import Categories, Area, Faskes, Option, Survey, Question, Answer
 
-# Register your models here.
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 1
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+    readonly_fields = ('get_survey_title', 'get_survey_faskes', 'get_question_text')
+
+    def get_survey_title(self, obj):
+        return obj.question.survey.judul
+    get_survey_title.short_description = 'Survey Title'
+
+    def get_survey_faskes(self, obj):
+        return obj.question.survey.faskes.nama if obj.question.survey.faskes else '-'
+    get_survey_faskes.short_description = 'Survey Faskes'
+
+    def get_question_text(self, obj):
+        return obj.question.question_text
+    get_question_text.short_description = 'Question Text'
+
+class FaskesInline(admin.TabularInline):
+    model = Faskes
+    extra = 1
+    inlines = [QuestionInline]
+
+class SurveyInline(admin.TabularInline):
+    model = Survey
+    extra = 1
+    inlines = [QuestionInline]
+
+class AreaInline(admin.TabularInline):
+    model = Area
+    extra = 1
+    inlines = [FaskesInline]
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('judul', 'date_created', 'date_modified')
+    inlines = [OptionInline]
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'tipe', 'parent', 'date_created', 'date_modified')
+    inlines = [FaskesInline]
+
+class FaskesAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'lokasi', 'date_created', 'date_modified')
+    inlines = [SurveyInline]
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('judul', 'survey_id', 'approved_by', 'created_by', 'date_created', 'date_modified')
+    list_filter = ('approved_by', 'created_by', 'date_created', 'date_modified')
+    search_fields = ('judul', 'survey_id', 'approved_by__username', 'created_by__username')
+    inlines = [QuestionInline]
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'survey', 'date_created', 'date_modified')
+    inlines = [AnswerInline]
+
+# Register the models and their respective admins
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Faskes, FaskesAdmin)
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question, QuestionAdmin)
--- apps/elements/models.py
+++ apps/elements/models.py
@@ -1,3 +1,133 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+import random
+import string
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Categories(models.Model):
+    judul = models.CharField(max_length=200)
+    keterangan = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.judul
+    
+class Area(models.Model):
+    AREA_CATEGORY = (
+        ('0', 'Provinsi'),
+        ('1', 'Kabupaten'),
+        ('2', 'Kota'),
+    )
+    nama = models.CharField(max_length=200)
+    kode = models.CharField(max_length=10, blank=True, null=True)
+    tipe = models.CharField(max_length=1, choices=AREA_CATEGORY)
+    parent = models.ForeignKey("self", limit_choices_to={'tipe': '0'}, verbose_name="Parent Area", on_delete=models.CASCADE, blank=True, null=True)
+    keterangan = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.nama)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nama
+
+class Faskes(models.Model):
+    nama = models.CharField(max_length=200)
+    lokasi = models.ForeignKey(Area, verbose_name="Lokasi Faskes", on_delete=models.CASCADE)
+    keterangan = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nama
+    
+
+class Option(models.Model):
+    nama = models.CharField(max_length=250)
+    nomor = models.IntegerField()
+    kategori = models.ForeignKey("Categories", on_delete=models.CASCADE, verbose_name="Kategori Nilai")
+    keterangan = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nama
+
+class Survey(models.Model):
+    STATUS_CHOICES = (
+        ('approved', 'Approved'),
+        ('reviewed', 'Reviewed'),
+        ('rejected', 'Rejected'),
+    )
+
+    judul = models.CharField(max_length=250)
+    survey_id = models.CharField(max_length=10, unique=True)
+    faskes = models.ForeignKey(Faskes, on_delete=models.CASCADE, null=True, blank=True)
+    keterangan = models.TextField(blank=True, null=True)
+    approved_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='approved_surveys')
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='created_surveys')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        request = kwargs.pop('request', None)
+
+        if request:
+            if not self.id:  # New instance, set created_by and created timestamp
+                self.created_by = request.user
+            else:  # Existing instance, set approved_by and modified timestamp
+                self.approved_by = request.user
+                self.date_modified = timezone.now()
+
+        if not self.survey_id:
+            self.survey_id = self.generate_unique_survey_id()
+
+        super(Survey, self).save(*args, **kwargs)
+
+    def generate_unique_survey_id(self):
+        length = 9
+        while True:
+            random_id = ''.join(random.choices(string.digits, k=length))
+            if not Survey.objects.filter(survey_id=random_id).exists():
+                return random_id
+
+    def set_status(self, new_status, user):
+        self.status = new_status
+        self.approved_by = user
+        self.save()
+
+    def __str__(self):
+        return self.judul
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name="judul survey")
+    question_text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    CHOICES = (
+        (1, 'Ya'),
+        (0, 'Tidak'),
+        (99, 'Tidak Menjawab'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='survey_answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.IntegerField(choices=CHOICES)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Answer by {self.user.username} to '{self.survey.title}'"
+
+    
